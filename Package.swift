b98d08616b0bc6a3506e4c92d15b409411fd344c
phh7590 <phh7590@gmail.com>
2023-01-14 13:10:53 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftyDraw",
     platforms: [
-        .iOS(.v9)
+        .iOS(.v13)
     ],
     products: [
         .library(name: "SwiftyDraw", targets: ["SwiftyDraw"])
